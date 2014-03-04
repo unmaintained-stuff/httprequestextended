a3794b573563056cca76802cdc527e7025aa3c6b
Oliver Hoff <oliver@hofff.com>
2014-03-04 20:29:11 +0100

    add contao 3 maintenance compat and clean up existing pruning

--- system/modules/httprequestextended/config/config.php
+++ system/modules/httprequestextended/config/config.php
@@ -5,11 +5,18 @@
  * @copyright	CyberSpectrum 2011
  * @author		Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @package		RequestExtended
- * @license		LGPL 
+ * @license		LGPL
  * @filesource
  */
 
-$GLOBALS['TL_CACHE']['requests']='tl_requestcache';
+// contao 2.x
+$GLOBALS['TL_CACHE']['requests'] = 'tl_requestcache';
+
+// contao 3.x
+$GLOBALS['TL_PURGE']['tables']['httprequestextended'] = array(
+	'callback' => array('RequestPruner', 'purgeRequestCache'),
+	'affected' => array('tl_requestcache'),
+);
 
 /**
  * Cron jobs
